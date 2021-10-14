75ddc577b1ca6880c8b81cbc59457c3ef31130ad
AnandDhakane01 <ananddhakane01@gmail.com>
2021-10-14 16:01:54 +0530

    conf

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on  port 3000");
 });
 module.exports = app;
