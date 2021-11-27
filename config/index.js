309e123e952eca319b938ed6b2db36f74da96c53
AnandDhakane01 <ananddhakane01@gmail.com>
2021-11-27 19:17:23 +0530

    local db setup

--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-    URI: process.env.URI,
+  URI: process.env.URI,
+  DB_USER: process.env.DB_USER,
+  HOST: process.env.HOST,
+  DATABASE: process.env.DATABASE,
+  PASSWORD: process.env.PASSWORD,
+  PORT: process.env.PORT,
 };
--- database/index.js
+++ database/index.js
@@ -1,18 +1,25 @@
 const { Sequelize } = require("sequelize");
 
-const { URI } = require("../config/index");
+const { DATABASE, DB_USER, PASSWORD, HOST, PORT } = require("../config/index");
 
 const sequelize = new Sequelize(process.env.URI);
+
+// connecting db on local
+// const sequelize = new Sequelize(DATABASE, DB_USER, PASSWORD, {
+//   HOST,
+//   PORT,
+//   dialect: "postgres",
+// });
 
 sequelize.sync();
 
 (async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection with DB established");
-    } catch (err) {
-        console.error("Unable to connect with DB");
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection with DB established");
+  } catch (err) {
+    console.error("Unable to connect with DB", err);
+  }
 })();
 
 module.exports = sequelize;
